9a5d3bdadbfce2aeb56d233db24a2e3bc53b30c4
RaulTeles <telesraul@gmail.com>
2025-01-13 18:20:41 -0300

    Service layer

--- basicProject/basicProject/src/main/java/com/teles/basicProject/repositories/UserRepository.java
+++ basicProject/basicProject/src/main/java/com/teles/basicProject/repositories/UserRepository.java
@@ -2,6 +2,8 @@ package com.teles.basicProject.repositories;
 
 import com.teles.basicProject.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User,Long> {
 }
--- basicProject/basicProject/src/main/java/com/teles/basicProject/resources/UserResource.java
+++ basicProject/basicProject/src/main/java/com/teles/basicProject/resources/UserResource.java
@@ -1,20 +1,36 @@
 package com.teles.basicProject.resources;
 
 import com.teles.basicProject.entities.User;
+import com.teles.basicProject.services.UserServices;
+import jakarta.annotation.Resource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping(value = "/users")
+@Resource
 public class UserResource {
+
+    @Autowired
+    private UserServices userServices;
 
 //    Respostas de requisiões webs
     @GetMapping()
-    public ResponseEntity<User> findAll(){
-        User u = new User(1L,"Teles","telesraul@gmail.com","81986614229","1234");
-        return ResponseEntity.ok().body(u);
+    public ResponseEntity<List<User>> findAll(){
+        List<User> list = userServices.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id){
+        User user = userServices.findById(id);
+        return ResponseEntity.ok().body(user);
     }
 }
 
