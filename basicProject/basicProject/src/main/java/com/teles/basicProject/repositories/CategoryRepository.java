9233c32f2ef0a07c0ef484b72f5eaa6734678061
RaulTeles <telesraul@gmail.com>
2025-01-15 10:42:56 -0300

    created category class

---
+++ basicProject/basicProject/src/main/java/com/teles/basicProject/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.teles.basicProject.repositories;
+
+import com.teles.basicProject.entities.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category,Long> {
+}
---
+++ basicProject/basicProject/src/main/java/com/teles/basicProject/resources/CategoryResource.java
@@ -0,0 +1,34 @@
+package com.teles.basicProject.resources;
+
+import com.teles.basicProject.entities.Category;
+import com.teles.basicProject.services.CategoryService;
+import jakarta.annotation.Resource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@Resource
+@RestController
+@RequestMapping(value = "/categories")
+public class CategoryResource {
+
+    @Autowired
+    public CategoryService categoryService;
+
+    @GetMapping
+    public ResponseEntity<List<Category>> findAll(){
+        List<Category> categories = categoryService.findAll();
+        return ResponseEntity.ok().body(categories);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Category> findById(@PathVariable Long id){
+        Category category = categoryService.findById(id);
+        return ResponseEntity.ok().body(category);
+    }
+}
