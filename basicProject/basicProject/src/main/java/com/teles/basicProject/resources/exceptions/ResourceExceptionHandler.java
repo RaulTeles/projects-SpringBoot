3db7c41281bedf794449468ebdff393eff213d81
RaulTeles <telesraul@gmail.com>
2025-01-16 17:14:07 -0300

    Exception Database error

--- basicProject/basicProject/src/main/java/com/teles/basicProject/resources/exceptions/ResourceExceptionHandler.java
+++ basicProject/basicProject/src/main/java/com/teles/basicProject/resources/exceptions/ResourceExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.teles.basicProject.resources.exceptions;
 
+import com.teles.basicProject.services.exceptions.DataBaseException;
 import com.teles.basicProject.services.exceptions.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
@@ -17,6 +18,13 @@ public class ResourceExceptionHandler extends RuntimeException {
     public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request){
         String error = "Resource not found";
         HttpStatus status = HttpStatus.NOT_FOUND;
+        StandardError err = new StandardError(Instant.now(),status.value(),error,e.getMessage(),request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+    @ExceptionHandler(DataBaseException.class)
+    public ResponseEntity<StandardError> dataBase(DataBaseException e, HttpServletRequest request){
+        String error = "Database error";
+        HttpStatus status = HttpStatus.BAD_REQUEST;
         StandardError err = new StandardError(Instant.now(),status.value(),error,e.getMessage(),request.getRequestURI());
         return ResponseEntity.status(status).body(err);
     }
--- basicProject/basicProject/src/main/java/com/teles/basicProject/services/exceptions/ResourceNotFoundException.java
+++ basicProject/basicProject/src/main/java/com/teles/basicProject/services/exceptions/ResourceNotFoundException.java
@@ -4,7 +4,7 @@ import java.io.Serial;
 
 public class ResourceNotFoundException extends RuntimeException{
     @Serial
-    public static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
     public ResourceNotFoundException(Object id){
         super("Resource not Found. Id "+ id);
