31a9d3c58fd4cf531e0bb67f1a1a175d0fbbfa94
Raul Teles <telesraul@gmail.com>
2025-01-24 13:41:17 -0300

    Inital commit of project

---
+++ project-devdojo/src/main/java/br/com/raulteles/project_devdojo/ProjectDevDojoApplication.java
@@ -0,0 +1,13 @@
+package br.com.raulteles.project_devdojo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectDevDojoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectDevDojoApplication.class, args);
+	}
+
+}
---
+++ project-devdojo/src/test/java/br/com/raulteles/project_devdojo/ProjectDevDojoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.raulteles.project_devdojo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectDevDojoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
